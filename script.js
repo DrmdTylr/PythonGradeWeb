81dffba07bc23129ea41ab6cbbe12a4854404180
DrmdTylr <drmdtylr@gmail.com>
2024-04-18 19:32:11 +0100

    total marks

--- script.js
+++ script.js
@@ -6,4 +6,37 @@ function grade_ammend() {
     totalGrade = finalGradeIncr+canMarks
     output_div = document.getElementById("output")
     output_div.textContent = Math.round((totalGrade/outOf)*100)
+
+    totalmarks = parseInt(document.getElementById("total").value)
+    if (totalmarks == null) {
+        return
+    }
+    else {
+        marks = totalmarks * (totalGrade/100)
+        output1_div = document.getElementById("output1")
+        output1_div.textContent = "Total marks: " + Math.round(marks)
+    }
 }
+
+function showInput() {
+    var checkbox = document.getElementById("check").checked
+
+    if (checkbox == true) {
+        document.getElementById("h2mark").style.display = "block"
+        document.getElementById("total").style.display = "block"
+    }
+    else if (checkbox == false) {
+        document.getElementById("h2mark").style.display = "none"
+        document.getElementById("total").style.display = "none"
+    }
+}
+
+
+
+fetch("./header/header.html")
+.then(response => {
+  return response.text()
+})
+.then(data => {
+  document.getElementById("myTopnav").innerHTML = data;
+});
