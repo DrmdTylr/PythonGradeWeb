230a9887ac844397fd324dcece1ba308898b6a05
DrmdTylr <drmdtylr@gmail.com>
2024-04-18 18:31:45 +0100

    New formula + bootstrap

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function grade_ammend() {
     input_text = parseInt(document.getElementById("grade").value)
     outOf=100
     canMarks=outOf*(input_text/100)
-    finalGradeIncr = (11.068*(outOf/100)) - (0.084*(canMarks))
+    finalGradeIncr = (12.67*(outOf/100)) - (0.0756*(canMarks))
     totalGrade = finalGradeIncr+canMarks
     output_div = document.getElementById("output")
     output_div.textContent = Math.round((totalGrade/outOf)*100)
